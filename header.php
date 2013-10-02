81356637459404ba33a6b99f2a3bd900568dde8b
Johann <johann.arispe@gmail.com>
2013-10-02 05:37:12 -0300

    Cambios Imagen logo

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 	<meta property="og:description" content="José works with video and photography, trying to catch these feelings and using the image as a mirror or as a witness of the experimentation of this labour. He looks for a relationship between the body and the image recorded." />
 	<meta property="description" content="José works with video and photography, trying to catch these feelings and using the image as a mirror or as a witness of the experimentation of this labour. He looks for a relationship between the body and the image recorded." />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="<?php echo get_ds_option('main_logo'); ?>" /> <?php } ?>
+	<meta property="og:image" content=" "http://josephart.me/wp-content/uploads/2013/09/josephart_logo.png" <?php //echo get_ds_option('main_logo'); ?> /> <?php } ?>
 	<?php 
 	$ds_gcode = get_ds_option('google_fonts_code');
 	if($ds_gcode) {
